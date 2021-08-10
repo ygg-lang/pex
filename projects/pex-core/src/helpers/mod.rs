697f0a3d96d35233f38fa348e495fc31fdc3ce55
Aster <aster@vers.site>
2021-08-10 23:07:59 +0000

    🐤 Make the `from_str` function from state parser

--- projects/pex-core/src/helpers/mod.rs
+++ projects/pex-core/src/helpers/mod.rs
@@ -1,4 +1,5 @@
-use crate::{ParseResult, ParseState};
+#![doc = include_str!("readme.md")]
+use crate::{ParseResult, ParseState, StopBecause};
 
 /// Match ascii whitespace and newlines, never fails
 ///
@@ -28,4 +29,39 @@ pub fn ascii_whitespace(state: ParseState) -> ParseResult<Box<str>> {
 pub fn whitespace(state: ParseState) -> ParseResult<Box<str>> {
     let len = state.rest_text.find(|c: char| !c.is_whitespace()).unwrap_or(0);
     state.advance_view(len).map_inner(|s| Box::from(s))
+}
+
+/// Make the [`from_str`](std::str::FromStr) function from state parser
+///
+/// # Arguments
+///
+/// * `state`:
+/// * `parser`:
+///
+/// returns: Result<T, StopBecause>
+///
+/// # Examples
+///
+/// ```no_run
+/// # use std::str::FromStr;
+/// # use pex::{helpers::{make_from_str, whitespace}, ParseState, StopBecause};
+/// impl FromStr for Compound {
+///     type Err = StopBecause;
+///
+///     fn from_str(s: &str) -> Result<Self, StopBecause> {
+///         // ignore whitespace at the start and end
+///         let state = ParseState::new(s.trim_end()).skip(whitespace);
+///         make_from_str(state, Self::parse)
+///     }
+/// }
+/// ```
+pub fn make_from_str<T, F>(state: ParseState, parser: F) -> Result<T, StopBecause>
+where
+    F: FnOnce(ParseState) -> ParseResult<T>,
+{
+    match parser(state) {
+        ParseResult::Pending(state, compound) if state.is_empty() => Ok(compound),
+        ParseResult::Pending(state, ..) => Err(StopBecause::ExpectEof { position: state.start_offset }),
+        ParseResult::Stop(e) => Err(e),
+    }
 }
