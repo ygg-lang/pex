58f775f4793a6936b3c0caedf864e5bed35789c9
Aster <aster@vers.site>
2021-08-23 09:00:03 +0000

    🚀 Export compile time `TrieSet`

--- projects/pex-trie/src/scripts/mod.rs
+++ projects/pex-trie/src/scripts/mod.rs
@@ -1,30 +1,24 @@
 use crate::generate::xid::XID_START;
 use std::{
     collections::BTreeSet,
-    fmt::{Display, Formatter},
+    fmt::{Debug, Formatter},
 };
 use ucd_trie::{Error, TrieSetOwned, TrieSetSlice};
 
-pub fn build_slice(ranges: &'static [(char, char)]) -> ucd_trie::Result<TrieSetSlice<'static>> {
-    unsafe {
-        let slice = owned.as_slice();
-        // elide the lifetime
-        Ok(std::mem::transmute(slice))
-    }
-}
-
-pub struct TrieSet {
+pub struct UnicodeSet {
+    name: String,
+    count: usize,
     max_width: usize,
     trie: TrieSetOwned,
 }
 
-impl Display for TrieSet {
+impl Debug for UnicodeSet {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        todo!()
+        f.debug_struct("UnicodeSet").field("name", &self.name).field("count", &self.count).finish()
     }
 }
 
-impl TrieSet {
+impl UnicodeSet {
     pub fn from_ranges(ranges: &[(char, char)]) -> Result<Self, Error> {
         let mut set = BTreeSet::new();
         for (start, end) in ranges {
@@ -32,12 +26,27 @@ impl TrieSet {
                 set.insert(i);
             }
         }
+        let count = set.len();
         let owned = TrieSetOwned::from_scalars(set)?;
-        Ok(Self { max_width: 144, trie: owned })
+        Ok(Self { name: "UNICODE_SET".to_string(), max_width: 144, count, trie: owned })
+    }
+    pub fn export_rust_code(&self) -> String {
+        let mut code = String::new();
+        code.push_str("pub static UNICODE_SET: TrieSetSlice<'static> = TrieSetSlice {\n");
+        code
     }
 }
 
+pub const UNICODE_SET: TrieSetSlice<'static> = TrieSetSlice {
+    tree1_level1: &[],
+    tree2_level1: &[],
+    tree2_level2: &[],
+    tree3_level1: &[],
+    tree3_level2: &[],
+    tree3_level3: &[],
+};
+
 #[test]
 fn test() {
-    println!("{:?}", build_slice(XID_START));
+    println!("{:?}", UnicodeSet::from_ranges(XID_START));
 }
